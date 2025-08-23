4e5331d7f297837095aa827ece922bf672480514
anuj9631 <anuj98426@gmail.com>
2025-08-23 17:37:53 +0530

    initited footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
+      
+      <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
