0b6e1f992102a4c728e2b72061bf58df1ec27268
anuj9631 <anuj98426@gmail.com>
2025-08-24 12:43:26 +0530

    Website complted with Transition

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Title from "./Title";
 import assets from "../assets/assets";
 import toast from "react-hot-toast";
+import { motion } from "motion/react"
 
 const ContactUs = () => {
 
@@ -35,7 +36,11 @@ const onSubmit = async (event) => {
 
 
   return (
-    <div
+    <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{once: true}}
+    transition={{staggerChildren: 0.2}}
       id="contact-us"
       className="flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
     >
@@ -43,7 +48,12 @@ const onSubmit = async (event) => {
         title="Reach out to us"
         desc="From strategy to execution, we craft digital solutions that move your business forward."
       />
-      <form onSubmit={onSubmit} className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+      <motion.form 
+       initial={{opacity:0, y:30}}
+whileInView={{opacity:1, y:0}}
+transition={{opacity: 0.5, delay: 0.4 }}
+viewport={{once:true}}
+      onSubmit={onSubmit} className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
         <div>
           <p className="mb-2 text-sm font-medium">Your name</p>
           <div className="flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600">
@@ -69,8 +79,8 @@ const onSubmit = async (event) => {
 
 </button>
 
-      </form>
-    </div>
+      </motion.form>
+    </motion.div>
   );
 };
 
